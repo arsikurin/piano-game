fb6eb71b9d5348f9371042c2de4331bdeb67cfe9
arsikurin <arseny.kurin@gmail.com>
2021-05-22 19:14:52 +0300

    added music and db support

--- main.py
+++ main.py
@@ -9,54 +9,35 @@ if DEBUG:
 else:
     log.basicConfig(format=f"{Fg.Green}{Style.Bold}%(asctime)s{Fg.Reset} %(message)s{Style.Reset}")
 
-with sqlite3.connect("users.sqlite") as conn:
-    log.debug("connected to db")
-    # c = conn.cursor()
-    # nickname = input(f"{Style.Bold}Enter nickname1 ->{Style.Reset}")
-    #
-    # if not get_user(nickname, conn, c):
-    #     init_user(nickname, conn, c)
-    #     log.debug("initializing user")
-    #     if (password := input(f"{Style.Bold}Enter your password ->{Style.Reset}")) != input(
-    #             f"{Style.Bold}Enter your password again ->{Style.Reset}"):
-    #         delete_user(nickname, conn, c)
-    #         print(f"{Fg.Red}Passwords do not match!")
-    #         print(Style.Bold + "Restart the game" + Style.Reset)
-    #         sys.exit()
-    #     else:
-    #         log.debug("password set")
-    #         update_password(password, nickname, conn, c)
-    #
-    # else:
-    #     log.debug("user found")
-    #     if input(f"{Style.Bold}Enter your password ->{Style.Reset}") != get_user(nickname, conn, c)[1]:
-    #         print(f"{Fg.Red}Passwords do not match!")
-    #         print(f"{Style.Bold}Restart the game" + Style.Reset)
-    #         sys.exit()
-    # log.debug("user in")
 
-    pygame.init()
-    menu_surface = create_example_window("Piano - menu", (600, 400))
+def start():
+    main(user_nickname)
 
-    try:
-        menu = pygame_menu.Menu(
-            height=400,
-            theme=pygame_menu.themes.THEME_SOLARIZED_DARK,
-            title="Piano",
-            width=600
-        )
-    except AttributeError:
-        log.debug("THEME_SOLARIZED_DARK not found. Switching to THEME_DARK")
-        menu = pygame_menu.Menu(
-            height=400,
-            theme=pygame_menu.themes.THEME_DARK,
-            title="Piano",
-            width=600
-        )
 
-    user_name = menu.add.text_input("Nickname: ", default="3p1c Cl1ck3r", maxchar=24)
-    # menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-    menu.add.button("Play", main)
-    menu.add.button("Quit", pygame_menu.events.EXIT)
-    log.debug("menu set")
-    menu.mainloop(menu_surface)
+pygame.init()
+pygame.font.init()
+pygame.mixer.init()
+menu_surface = create_example_window("Piano - menu", (600, 400))
+
+try:
+    menu = pygame_menu.Menu(
+        height=400,
+        theme=pygame_menu.themes.THEME_SOLARIZED_DARK,
+        title="Piano",
+        width=600
+    )
+except AttributeError:
+    log.debug("THEME_SOLARIZED_DARK not found. Switching to THEME_DARK")
+    menu = pygame_menu.Menu(
+        height=400,
+        theme=pygame_menu.themes.THEME_DARK,
+        title="Piano",
+        width=600
+    )
+
+user_nickname = menu.add.text_input("Nickname: ", default="3p1c Cl1ck3r", maxchar=24)
+# menu.add.selector("Difficulty :", [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add.button("Play", start)
+menu.add.button("Quit", pygame_menu.events.EXIT)
+log.debug("menu set")
+menu.mainloop(menu_surface)
--- piano_api.py
+++ piano_api.py
@@ -8,185 +8,211 @@ import random
 import time
 
 
-def main() -> None:
-    class Piano:
-        pygame.font.init()
-        clock = pygame.time.Clock()
-        key_height = 120
-        size = width, height = 600, 1000
-        pygame.display.set_caption("Piano - game")
-        surface = pygame.display.set_mode(size)
-        amount = 0
-        score = 0
-        rects = []
-        surface.fill((255, 255, 255))
-        pygame.draw.line(surface, (128, 128, 128), [200, 0], [200, 1000], 5)
-        pygame.draw.line(surface, (128, 128, 128), [400, 0], [400, 1000], 5)
-        pygame.draw.line(surface, (128, 128, 128), [0, 52], [600, 52], 5)
-        pygame.draw.line(surface, (255, 0, 0), [0, 800], [600, 800], 5)
-        pygame.display.update()
-        jump = 1
-        log.debug("game set")
+def main(user_nickname) -> None:
+    with sqlite3.connect("users.sqlite") as conn:
+        log.debug("connected to db")
+        c = conn.cursor()
+        if not get_user((user_nickname := user_nickname.get_value()), conn, c):
+            init_user(user_nickname, conn, c)
+            log.debug("initializing user")
+        else:
+            log.debug("user found")
 
-        @classmethod
-        def handle_events(cls) -> None:
-            """
-            handles events
-            """
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+        class Piano:
+            pygame.display.set_caption("Piano - game")
+            clock = pygame.time.Clock()
+            key_height = 120
+            window_width, window_height = 600, 1000
+            times = np.linspace(0.5, 0.6)
+            surface = pygame.display.set_mode((window_width, window_height))
+            keys_amount = 0
+            score = 0
+            rects = []
+            jump = 1
 
-                elif event.type == pygame.KEYDOWN and event.key == 49:
-                    for rect in cls.rects:
-                        mid, bottom = rect.midbottom
-                        if mid < 100 and bottom >= 800 >= bottom - cls.key_height:
-                            pygame.draw.rect(cls.surface, (255, 255, 255), rect)
-                            cls.rects.remove(rect)
-                            cls.amount -= 1
-                            cls.score += 1
-                            log.debug("LEFT")
-                        # else:
-                        #     log.debug("MISS")
+            surface.fill((255, 255, 255))
+            pygame.draw.line(surface, (128, 128, 128), [200, 0], [200, 1000], 5)
+            pygame.draw.line(surface, (128, 128, 128), [400, 0], [400, 1000], 5)
+            pygame.draw.line(surface, (128, 128, 128), [0, 52], [600, 52], 5)
+            pygame.draw.line(surface, (255, 0, 0), [0, 800], [600, 800], 5)
+            pygame.display.update()
 
-                elif event.type == pygame.KEYDOWN and event.key == 50:
-                    for rect in cls.rects:
-                        mid, bottom = rect.midbottom
-                        if 400 > mid > 100 and bottom >= 800 >= bottom - cls.key_height:
-                            pygame.draw.rect(cls.surface, (255, 255, 255), rect)
-                            cls.rects.remove(rect)
-                            cls.amount -= 1
-                            cls.score += 1
-                            log.debug("MID")
-                        # else:
-                        #     log.debug("MISS")
+            @classmethod
+            def handle_events(cls) -> None:
+                """
+                handles events
+                """
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        if highest_score < Piano.score:
+                            update_points(Piano.score, user_nickname, conn, c)
+                        pygame.quit()
+                        sys.exit()
 
-                elif event.type == pygame.KEYDOWN and event.key == 51:
-                    for rect in cls.rects:
-                        mid, bottom = rect.midbottom
-                        if mid > 500 and bottom >= 800 >= bottom - cls.key_height:
-                            pygame.draw.rect(cls.surface, (255, 255, 255), rect)
-                            cls.rects.remove(rect)
-                            cls.amount -= 1
-                            cls.score += 1
-                            log.debug("RIGHT")
-                        # else:
-                        #     log.debug("MISS")
+                    elif event.type == pygame.KEYDOWN and event.key == 49:
+                        for rect in cls.rects:
+                            mid, bottom = rect.midbottom
+                            if mid < 100 and bottom >= 800 >= bottom - cls.key_height:
+                                pygame.draw.rect(cls.surface, (255, 255, 255), rect)
+                                cls.rects.remove(rect)
+                                cls.keys_amount -= 1
+                                cls.score += 1
+                                log.debug("LEFT")
+                            # else:
+                            #     log.debug("MISS")
 
-        @classmethod
-        def left_create(cls) -> None:
-            """
-            creates left key
-            """
-            rect = pygame.rect.Rect((0, 50, 198, cls.key_height))
-            pygame.draw.rect(cls.surface, (0, 0, 0), rect)
-            cls.rects.append(rect)
-            cls.amount += 1
+                    elif event.type == pygame.KEYDOWN and event.key == 50:
+                        for rect in cls.rects:
+                            mid, bottom = rect.midbottom
+                            if 400 > mid > 100 and bottom >= 800 >= bottom - cls.key_height:
+                                pygame.draw.rect(cls.surface, (255, 255, 255), rect)
+                                cls.rects.remove(rect)
+                                cls.keys_amount -= 1
+                                cls.score += 1
+                                log.debug("MID")
+                            # else:
+                            #     log.debug("MISS")
 
-        @classmethod
-        def middle_create(cls) -> None:
-            """
-            creates middle key
-            """
-            rect = pygame.rect.Rect((203, 50, 195, cls.key_height))
-            pygame.draw.rect(cls.surface, (0, 0, 0), rect)
-            cls.rects.append(rect)
-            cls.amount += 1
+                    elif event.type == pygame.KEYDOWN and event.key == 51:
+                        for rect in cls.rects:
+                            mid, bottom = rect.midbottom
+                            if mid > 500 and bottom >= 800 >= bottom - cls.key_height:
+                                pygame.draw.rect(cls.surface, (255, 255, 255), rect)
+                                cls.rects.remove(rect)
+                                cls.keys_amount -= 1
+                                cls.score += 1
+                                log.debug("RIGHT")
+                            # else:
+                            #     log.debug("MISS")
 
-        @classmethod
-        def right_create(cls) -> None:
-            """
-            creates right key
-            """
-            rect = pygame.rect.Rect((403, 50, 200, cls.key_height))
-            pygame.draw.rect(cls.surface, (0, 0, 0), rect)
-            cls.rects.append(rect)
-            cls.amount += 1
-
-        @classmethod
-        def move_keys(cls) -> None:
-            """
-            moves keys
-            """
-            for rect in cls.rects:
-                pygame.draw.rect(cls.surface, (255, 255, 255), rect)
-                rect.move_ip(0, cls.jump)
+            @classmethod
+            def draw_left_key(cls) -> None:
+                """
+                creates left key
+                """
+                rect = pygame.rect.Rect((0, 50, 198, cls.key_height))
                 pygame.draw.rect(cls.surface, (0, 0, 0), rect)
-                pygame.draw.line(cls.surface, (255, 0, 0), [0, 800], [600, 800], 5)
-                pygame.draw.line(cls.surface, (128, 128, 128), [0, 52], [600, 52], 5)
-                if rect.midbottom[1] > 1150:
+                cls.rects.append(rect)
+                cls.keys_amount += 1
+
+            @classmethod
+            def draw_middle_key(cls) -> None:
+                """
+                creates middle key
+                """
+                rect = pygame.rect.Rect((203, 50, 195, cls.key_height))
+                pygame.draw.rect(cls.surface, (0, 0, 0), rect)
+                cls.rects.append(rect)
+                cls.keys_amount += 1
+
+            @classmethod
+            def draw_right_key(cls) -> None:
+                """
+                creates right key
+                """
+                rect = pygame.rect.Rect((403, 50, 200, cls.key_height))
+                pygame.draw.rect(cls.surface, (0, 0, 0), rect)
+                cls.rects.append(rect)
+                cls.keys_amount += 1
+
+            @classmethod
+            def move_keys(cls) -> None:
+                """
+                moves keys
+                """
+                for rect in cls.rects:
                     pygame.draw.rect(cls.surface, (255, 255, 255), rect)
-                    cls.amount -= 1
-                    log.debug("missed a key")
-                    cls.rects.remove(rect)
-                    if cls.score:
-                        cls.score -= 1
+                    rect.move_ip(0, cls.jump)
+                    pygame.draw.rect(cls.surface, (0, 0, 0), rect)
+                    pygame.draw.line(cls.surface, (255, 0, 0), [0, 800], [600, 800], 5)
+                    pygame.draw.line(cls.surface, (128, 128, 128), [0, 52], [600, 52], 5)
+                    if rect.midbottom[1] > 1150:
+                        pygame.draw.rect(cls.surface, (255, 255, 255), rect)
+                        cls.keys_amount -= 1
+                        log.debug("missed a key")
+                        cls.rects.remove(rect)
+                        if cls.score:
+                            cls.score -= 1
 
-        @classmethod
-        def clear_rects(cls) -> None:
-            """
-            clears excess keys
-            """
-            if cls.amount > 20:
-                pygame.draw.rect(cls.surface, (255, 255, 255), cls.rects[0])
-                del cls.rects[0]
-                cls.amount -= 1
+            @classmethod
+            def clear_rects(cls) -> None:
+                """
+                clears excess keys
+                """
+                if cls.keys_amount > 20:
+                    pygame.draw.rect(cls.surface, (255, 255, 255), cls.rects[0])
+                    del cls.rects[0]
+                    cls.keys_amount -= 1
 
-    def generate_keys() -> None:
-        """
-        generates keys
-        """
-        log.debug("keys generator thread started")
-        while True:
-            time.sleep(float(random.choice(times)))
-            choose = random.randint(0, 2)
-            if choose == 0:
-                Piano.left_create()
-            elif choose == 1:
-                Piano.middle_create()
-            else:
-                Piano.right_create()
+            @classmethod
+            def set_music(cls, file: str) -> None:
+                pygame.mixer.music.load(file)
+                pygame.mixer.music.play(-1)
 
-    def speed_setter() -> None:
-        """
-        sets keys speed
-        """
-        global times
-        # if Piano.score < 10:
-        #     clock.tick(240)
-        # times = np.linspace(0.6, 0.9)
-        # elif Piano.score < 30:
-        Piano.clock.tick(360)
-        times = np.linspace(0.5, 0.6)
-        # elif Piano.score < 80:
-        #     Piano.jump = 2
-        #     clock.tick(260)
-        #     times = np.linspace(0.25, 0.3)
-        # elif Piano.score < 120:
-        #     Piano.jump = 4
-        #     clock.tick(240)
-        # elif Piano.score < 300:
-        #     Piano.jump = 4
-        #     clock.tick(360)
+            @classmethod
+            def generate_keys(cls) -> None:
+                """
+                generates new keys
+                """
+                log.debug("keys generator thread started")
+                while True:
+                    time.sleep(float(random.choice(cls.times)))
+                    choose = random.randint(0, 2)
+                    if choose == 0:
+                        Piano.draw_left_key()
+                    elif choose == 1:
+                        Piano.draw_middle_key()
+                    else:
+                        Piano.draw_right_key()
 
-    times = np.linspace(0.5, 0.6)
-    keys_generator_thread = threading.Thread(target=generate_keys, daemon=True)
-    keys_generator_thread.start()
+            @classmethod
+            def render_score(cls) -> None:
+                pygame.draw.rect(Piano.surface, (255, 255, 255), (0, 0, 600, 50))
+                score_text = segoeui_font.render(f"{highest_score} {Piano.score}", True, (255, 0, 0))
+                Piano.surface.blit(score_text, (2, 0))
 
-    log.debug("game thread started")
-    while True:
+            @classmethod
+            def set_speed(cls) -> None:
+                """
+                sets keys speed
+                """
+                # if Piano.score < 10:
+                #     clock.tick(240)
+                # cls.times = np.linspace(0.6, 0.9)
+                # elif Piano.score < 30:
+                Piano.clock.tick(360)
+                cls.times = np.linspace(0.5, 0.6)
+                # elif Piano.score < 80:
+                #     Piano.jump = 2
+                #     clock.tick(260)
+                #     cls.times = np.linspace(0.25, 0.3)
+                # elif Piano.score < 120:
+                #     Piano.jump = 4
+                #     clock.tick(240)
+                # elif Piano.score < 300:
+                #     Piano.jump = 4
+                #     clock.tick(360)
+
+        keys_generator_thread = threading.Thread(target=Piano.generate_keys, daemon=True)
+        keys_generator_thread.start()
         segoeui_font = pygame.font.Font("/usr/share/fonts/WindowsFonts/segoeui.ttf", 36)
-        pygame.draw.rect(Piano.surface, (255, 255, 255), (0, 0, 600, 50))
-        score_text = segoeui_font.render(f"Score: {Piano.score}", True, (255, 0, 0))
-        Piano.surface.blit(score_text, (2, 0))
-        pygame.display.update()
+        highest_score = get_user(user_nickname, conn, c)[1]
+        Piano.set_music("stal-c418.wav")
 
-        speed_setter()
-        Piano.move_keys()
-        Piano.handle_events()
-        pygame.display.update()
-        Piano.clear_rects()
+        log.debug("game thread started")
+        try:
+            while True:
+                Piano.render_score()
+                Piano.set_speed()
+                Piano.move_keys()
+                Piano.handle_events()
+                Piano.clear_rects()
+                pygame.display.update()
+        except KeyboardInterrupt:
+            if highest_score < Piano.score:
+                update_points(Piano.score, user_nickname, conn, c)
+            pygame.quit()
+            sys.exit()
 
 
 def init_user(
@@ -203,65 +229,26 @@ def init_user(
     """
     with conn:
         c.execute(
-            "INSERT INTO users VALUES (:nickname, :password, :points, :lvl)",
-            {"nickname": nickname, "password": None, "points": None, "lvl": 1})
+            "INSERT INTO users VALUES (:nickname, :highest_points, :lvl)",
+            {"nickname": nickname, "highest_points": None, "lvl": 1})
 
 
-def update_password(
-        password: str,
+def get_user(
         nickname: str,
         conn: sqlite3.Connection,
         c: sqlite3.Cursor
-) -> None:
+) -> tuple:
     """
-    update user's password
+    get user data
 
     :param c: cursor
     :param conn: connection to db
-    :param password: user's password
     :param nickname: user's nickname
+    :return: user data
     """
     with conn:
-        c.execute("UPDATE users SET password = :password WHERE nickname = :nickname",
-                  {"password": password, "nickname": nickname})
-
-
-def update_lvl(
-        lvl: int,
-        nickname: str,
-        conn: sqlite3.Connection,
-        c: sqlite3.Cursor
-) -> None:
-    """
-    update user's level
-
-    :param c: cursor
-    :param conn: connection to db
-    :param lvl: user's level
-    :param nickname: user's nickname
-    """
-    with conn:
-        c.execute("UPDATE users SET lvl = :lvl WHERE nickname = :nickname",
-                  {"lvl": lvl, "nickname": nickname})
-
-
-def update_points(
-        points: int,
-        nickname: str,
-        conn: sqlite3.Connection,
-        c: sqlite3.Cursor
-) -> None:
-    """
-    update user's points
-
-    :param c: cursor
-    :param conn: connection to db
-    :param points: user's points
-    :param nickname: user's nickname
-    """
-    with conn:
-        c.execute("UPDATE users SET points = :points WHERE nickname = :nickname",
-                  {"points": points, "nickname": nickname})
+        c.execute("select * from users where nickname=:nickname", {"nickname": nickname})
+        return c.fetchone()
 
 
 def delete_user(
@@ -280,19 +267,39 @@ def delete_user(
         c.execute("delete from users where nickname=:nickname", {"nickname": nickname})
 
 
-def get_user(
+def update_lvl(
+        lvl: int,
         nickname: str,
         conn: sqlite3.Connection,
         c: sqlite3.Cursor
-) -> tuple:
+) -> None:
     """
-    get user data
+    update user's level
 
     :param c: cursor
     :param conn: connection to db
+    :param lvl: user's level
     :param nickname: user's nickname
-    :return: user data
     """
     with conn:
-        c.execute("SELECT * FROM users WHERE nickname=:nickname", {"nickname": nickname})
-        return c.fetchone()
+        c.execute("update users set lvl = :lvl where nickname = :nickname",
+                  {"lvl": lvl, "nickname": nickname})
+
+
+def update_points(
+        highest_points: int,
+        nickname: str,
+        conn: sqlite3.Connection,
+        c: sqlite3.Cursor
+) -> None:
+    """
+    update user's highest_points
+
+    :param c: cursor
+    :param conn: connection to db
+    :param highest_points: user's highest_points
+    :param nickname: user's nickname
+    """
+    with conn:
+        c.execute("update users set highest_points = :highest_points where nickname = :nickname",
+                  {"highest_points": highest_points, "nickname": nickname})
