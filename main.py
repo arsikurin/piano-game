2c2c421852187fd933f9701fed520c7750f98c65
arsikurin <arseny.kurin@gmail.com>
2021-05-29 20:59:16 +0300

    logging improved

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from piano_api import *
+from pygame_menu.themes import Theme
 from pygame_menu.examples import create_example_window
 
 DEBUG = True
 if DEBUG:
-    log.basicConfig(format=f"{Fg.Green}{Style.Bold}%(asctime)s{Fg.Reset} %(message)s{Style.Reset}", level=log.DEBUG)
+    log.basicConfig(format=f"{Fg.Green}{Style.Bold}%(asctime)s{Fg.Reset}{Style.Bold} %(message)s{Style.Reset}", level=log.DEBUG)
 else:
-    log.basicConfig(format=f"{Fg.Green}{Style.Bold}%(asctime)s{Fg.Reset} %(message)s{Style.Reset}")
+    log.basicConfig(format=f"{Fg.Green}{Style.Bold}%(asctime)s{Fg.Reset}{Style.Bold} %(message)s{Style.Reset}")
 
 
 def start() -> None:
@@ -32,21 +33,23 @@ pygame.font.init()
 pygame.mixer.init()
 menu_surface = create_example_window("Piano - menu", (600, 400))
 
-try:
-    menu = pygame_menu.Menu(
-        height=400,
-        theme=pygame_menu.themes.THEME_SOLARIZED_DARK,
-        title="Piano",
-        width=600
-    )
-except AttributeError:
-    log.debug("THEME_SOLARIZED_DARK not found. Switching to THEME_DARK")
-    menu = pygame_menu.Menu(
-        height=400,
-        theme=pygame_menu.themes.THEME_DARK,
-        title="Piano",
-        width=600
-    )
+THEME_SOLARIZED_DARK = Theme(
+    background_color=(47, 48, 51),
+    cursor_color=(123, 51, 86),
+    cursor_selection_color=(146, 160, 160, 120),
+    selection_color=(207, 62, 132),
+    title_background_color=(4, 47, 58),
+    title_font_color=(38, 158, 151),
+    widget_font_color=(152, 172, 180)
+)
+
+menu = pygame_menu.Menu(
+    height=400,
+    theme=THEME_SOLARIZED_DARK,
+    title="Piano",
+    width=600
+)
+
 user_nickname = menu.add.text_input("Nickname: ", default="3p1c Cl1ck3r", maxchar=24)
 menu.add.selector("Game mode: ", [("Endless", 1), ("Normal", 2), ("Hardcore", 3)], onchange=set_game_mode)
 menu.add.button("Play", start)
